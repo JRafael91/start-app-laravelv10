121f04e509f8872df2e319e32de65e4652613549
Rafael Acosta <rafa14494@gmail.com>
2023-02-20 00:17:17 -0700

    feat: install breeze with blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\ReplyController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +11,29 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', 'dashboard');
+
+Route::get('/dashboard', [CommentController::class, 'index'])
+->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::post('comments', [CommentController::class, 'store'])
+->name('comments.store')
+->middleware('auth');
+
+Route::post('replies/{comment}', [ReplyController::class, 'store'])
+->name('replies.store')
+->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
